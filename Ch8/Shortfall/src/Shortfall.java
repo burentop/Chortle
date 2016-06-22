e5c3422d021a098341440f35b2bde528547238bd
Brent Perry <burentoperii@gmail.com>
2016-06-22 11:54:46 -0500

    Change data type to int

--- Ch8/Shortfall/src/Shortfall.java
+++ Ch8/Shortfall/src/Shortfall.java
@@ -2,7 +2,7 @@
 public class Shortfall {
 	public static void main ( String[] args )
 	  {
-	    short value = 32;
-	    System.out.println("A short: " +  value);
+	    int value = 35000;
+	    System.out.println("An int: " +  value);
 	  }
 }
