e4da6695add51910955a696022fc26f4bbb89d64
Brent Perry <burentoperii@gmail.com>
2016-06-22 13:17:08 -0500

    Initial commit of CharAssassination

--- Ch8/ExponentialExplosion/src/ExponentialExplosion.java
+++ Ch8/ExponentialExplosion/src/ExponentialExplosion.java
@@ -2,7 +2,7 @@
 public class ExponentialExplosion {
 	public static void main ( String[] args )
 	  {
-	    double value = 32;
+	    double value = 3200;
 	    System.out.println("e to the power value: " +  Math.exp( value ) );
 	  }
 }
