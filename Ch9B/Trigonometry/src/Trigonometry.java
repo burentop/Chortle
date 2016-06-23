c1d0e55c9a770cc8f29e586bbfc99da26535e941
Brent Perry <burentoperii@gmail.com>
2016-06-23 12:41:32 -0500

    Change radian calculation.

--- Ch9B/Trigonometry/src/Trigonometry.java
+++ Ch9B/Trigonometry/src/Trigonometry.java
@@ -1,9 +1,9 @@
 
 public class Trigonometry {
 	public static void main (String[] args) {
-		double input = 0.5246;
-		double sine = Math.sin(Math.toRadians(input));
-		double cos = Math.cos(Math.toRadians(input));
+		double angle = 30;
+		double sine = Math.sin(angle * Math.PI/180);
+		double cos = Math.cos(angle * Math.PI/180);
 		double total = (sine * sine) + (cos * cos);
 		
 		System.out.println("Sine: " + sine + " Cosine: " + cos + " Sum: " + total);
