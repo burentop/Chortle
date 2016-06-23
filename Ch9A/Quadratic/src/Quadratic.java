024628f8c3e8ff2e2aec9193020841d46a088a75
Brent Perry <burentoperii@gmail.com>
2016-06-23 11:51:10 -0500

    Add multiple variables.

--- Ch9A/Quadratic/src/Quadratic.java
+++ Ch9A/Quadratic/src/Quadratic.java
@@ -1,11 +1,16 @@
 
 public class Quadratic {
 	public static void main(String[] args) {
-		double x = 3.14;
+		double x = 3.14, y = 4.2;
 		double result;
 		
-		result = (3 * (x * x)) - (8 * x) +4;
+		result = (3 * (x * x)) - (8 * y) +4;
 		
-		System.out.println("At X = " + x + " the value is " + result);
+		System.out.println("At X = " + x + " and Y = " + y + "the value is " + result);
+		
+		y = 0;
+		result = (3 * (x * x)) - (8 * y) +4;
+		
+		System.out.println("At X = " + x + " and Y = " + y + "the value is " + result);
 	}
 }
