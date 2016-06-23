50507cadcdf1e47efa7e701a8c3ff464e6e2cc0e
Brent Perry <burentoperii@gmail.com>
2016-06-23 16:22:47 -0500

    Add additional coins.

--- Ch10/CorrectChange/src/CorrectChange.java
+++ Ch10/CorrectChange/src/CorrectChange.java
@@ -10,8 +10,14 @@ public class CorrectChange {
 		cents = scan.nextInt();
 		dollar = cents / 100;
 		quarter = (cents % 100) / 25;
+		dime = (cents % 25) / 10;
+		nickel = (((cents % 100) % 25) % 10) / 5; 
+		penny = (cents % 5);
 		
-		System.out.println("Your change is: " + dollar + " dollar(s), " + quarter + " quarter(s)");
+		System.out.println("Your change is: " + dollar + " dollar(s), " + quarter + " quarter(s) " + dime + " dime(s) " +
+				nickel + " nickel(s) " + penny + " pennies.");
+		
+		scan.close();
 		
 	}
 }
