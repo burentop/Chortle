b386c0cb2125d24acd22badce36aadefdafd98e0
Brent Perry <burentoperii@gmail.com>
2016-06-23 11:45:38 -0500

    Complete formula for quadratic.

--- Ch9A/Quadratic/src/Quadratic.java
+++ Ch9A/Quadratic/src/Quadratic.java
@@ -1,4 +1,11 @@
 
 public class Quadratic {
-
+	public static void main(String[] args) {
+		double x = 3.14;
+		double result;
+		
+		result = (3 * (x * x)) - (8 * x) +4;
+		
+		System.out.println("At X = " + x + " the value is " + result);
+	}
 }
