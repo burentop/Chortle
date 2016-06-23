966ba7d8709fa64e0c4d1763b27edd69e9aafec9
Brent Perry <burentoperii@gmail.com>
2016-06-23 12:28:19 -0500

    Initial commit for AverageRainfall.

---
+++ Ch9B/AverageRainfall/src/AverageRainfall.java
@@ -0,0 +1,12 @@
+
+public class AverageRainfall {
+	public static void main (String[] args) {
+		int april = 12, may = 14, june = 8;
+		double average = (april + may + june) / 3;
+		
+		System.out.println("Rainfall for April: \t" + april);
+		System.out.println("Rainfall for May: \t" + may);
+		System.out.println("Rainfall for June: \t" + june);
+		System.out.println("Average rainfall: \t" + average);
+	}
+}
