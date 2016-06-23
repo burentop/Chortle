62993fdaf41cc03cf2497d7773679c15688fb0f1
Brent Perry <burentoperii@gmail.com>
2016-06-23 12:58:42 -0500

    Add second String variable.

--- Ch9C/StringObject/src/StringObject.java
+++ Ch9C/StringObject/src/StringObject.java
@@ -2,8 +2,11 @@
 public class StringObject {
 	public static void main (String[] args) {
 		String str;
+		String str2;
 		str = new String("Hello World.");
+		str2 = "Now it works.";
 		
 		System.out.println(str);
+		System.out.println(str2);
 	}
 }
